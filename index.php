d6995be19d0d6edf576dab671956cda2faf61f62
omar_casasolas_merida <omarcasasolasmerida@gmail.com>
2020-10-05 22:52:07 -0400

    Hola UMSS

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Asistencias UMSS</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
-<body>
-    <h2>UMSS TIS</h2>
+<body class="bg-secondary">
+    <main class="container bg-white mt-5 ">
+        <h2 class="text-center text-danger">Sistema de Asistencia Virtual UMSS</h2>
+    </main>
 </body>
 </html>
