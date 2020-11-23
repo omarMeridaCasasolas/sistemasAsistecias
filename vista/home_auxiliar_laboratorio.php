850ddb47fd3a190dd7fe10540ff1bad0388d3ad6
omar_casasolas_merida <omarcasasolasmerida@gmail.com>
2020-11-22 21:16:30 -0400

    subir archivos s3 v2

--- vista/home_auxiliar_laboratorio.php
+++ vista/home_auxiliar_laboratorio.php
@@ -250,7 +250,7 @@
         ?>
     </main>
 
-    <footer>
+    <footer class ="bg-white container">
             <h1>S3 upload example</h1>
             <?php
                 if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['userfile']) && $_FILES['userfile']['error'] == UPLOAD_ERR_OK && is_uploaded_file($_FILES['userfile']['tmp_name'])) {
